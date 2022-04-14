aaf32b84a8089bd6073d868440e0cea65a9921af
ShinDongJun <wq0212@naver.com>
2022-04-14 16:41:40 +0900

    add two logo page4

--- src/component/mainpage_section_4.js
+++ src/component/mainpage_section_4.js
@@ -10,6 +10,7 @@ class FullPageSection4 extends Component{
             <div className="section">
                 <div className="contactWrap">
                     <h1>Contacts</h1>
+                    <div className="logoBox">                    
                     <Logo logoName="Facebook" logoPath="img/icon/facebook.svg" logoLink="https://www.facebook.com/profile.php?id=100010290605875" background="#4267B2">신동준</Logo>
                     <Logo logoName="Instagram" logoPath="img/icon/instagram.svg" logoLink="https://www.instagram.com/dongjun0212/?hl=ko" background="#C13584">dongjun0212</Logo>
                     <Logo logoName="Blog" logoPath="img/icon/blog.svg" logoLink="https://pypystory.tistory.com/" background="#eb531f">PyPyStory</Logo>
@@ -17,6 +18,12 @@ class FullPageSection4 extends Component{
                     <Logo logoName="Youtube" logoPath="img/icon/youtube.svg" logoLink="https://www.youtube.com/channel/UCXgIxJNWOp3E6RfYcUuaH3g" background="#FF0000">PyPyStory</Logo>
                     <Logo logoName="Mail" logoPath="img/icon/gmail.svg" logoLink="#" background="#EA4335">wq0212<br />@naver.com</Logo>
                     <Logo logoName="Kakao Talk" logoPath="img/icon/kakao.svg" logoLink="https://open.kakao.com/o/skoddvae" background="#FFE812">Open Talk</Logo>
+                    </div>
+                    <div className="logoBox">
+                    <Logo logoName="Solved AC" logoPath="img/icon/solvedac.svg" logoLink="https://solved.ac/profile/wq0212" background="#17CE3A" logoSize="100" logoTop="0px">wq0212</Logo>
+                    <Logo logoName="Baekjoon" logoPath="img/icon/boj.svg" logoLink="https://www.acmicpc.net/user/wq0212" background="#0077C1" logoSize="100" logoTop="0px">wq0212</Logo>
+                    </div>
+                    
                 </div>
             </div>
         )
--- src/component/ui/logo.js
+++ src/component/ui/logo.js
@@ -3,27 +3,39 @@ import styled from "styled-components";
 const LogoWrap = styled.div`
 float: left;
 margin: 3%;
-
+margin-bottom: 0;
 `;
 const LogoIcon = styled.div`
 border-radius: 30px;
 width: 100px;
 height: 100px;
 text-align: center;
-padding-top: 22px;
+padding-top: ${(props) => props.logoTop || "22px"};;
 margin-bottom: 10px;
 background: ${(props) => props.background || "black"};
-`;
+box-shadow: 10px 10px 10px gray;
+`; //
 const LogoName = styled.div``;
 // const LogoLink = styled.div``;
-export default function Logo({logoPath,background,logoName,children,logoLink}) {
+export default function Logo({logoPath,background,logoName,children,logoLink,logoSize,logoTop}) {
+    if(logoSize==null){
+        logoSize = "50";
+    }
     return (
         <LogoWrap>
             <a href={logoLink} 
             target="_blank"
             rel="noopener noreferrer">
-            <LogoIcon background={background}><img src={logoPath} alt={logoName} width="50" height="auto"></img></LogoIcon></a>
-            <LogoName><p><a href={logoLink} target="_blank" rel="noopener noreferrer">{logoName}<br/>
+            <LogoIcon background={background} 
+            logoTop={logoTop}>
+                <img src={logoPath} 
+                alt={logoName} 
+                width={logoSize} 
+                height="auto"></img>
+            </LogoIcon></a>
+            <LogoName><p><a href={logoLink} 
+            target="_blank" 
+            rel="noopener noreferrer">{logoName}<br/>
             {children}</a></p></LogoName>
         </LogoWrap>
     );
