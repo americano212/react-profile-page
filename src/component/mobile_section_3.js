b7c7de6006258f854b5a5fbe0185e62a4d8ff79c
ShinDongJun <wq0212@naver.com>
2022-04-14 21:48:05 +0900

    make page 3 CSS

--- src/component/mobile_section_3.js
+++ src/component/mobile_section_3.js
@@ -1,21 +1,22 @@
 import React,{ Component } from "react";
 import '../css/fullpage-overrides.css';
-import '../css/mainpage_section_3.css';
+import '../css/mobile_section_3.css';
 import 'bootstrap/dist/css/bootstrap.css';
-
+import Badge from "./ui/badge";
 class MobileSection3 extends Component{
     render(){
         return (
             <div className="section">
-                <div className="projectWrap">
+                <div className="projectWrapM">
                     <h1>Projects</h1>
                     <br/>
-                    <div className="projectBox leftBox">
+                    <div className="projectBoxM">
                         <h4>Campus Compass</h4>
-                        <br/>
-                        <p>Sogang Univ. Artech Hackathon 1st (2021.12)</p>
-                        <p><div className="p_bold">Main Tech Stack</div> : Node.js + express, MySQL</p>
-                        <p><div className="p_bold">Summary</div> : '대학생활에서 궁금한 부분에 대한 길을 찾아주는 나침반'</p>
+                        <p>Sogang Univ. Artech Hackathon 1st</p>
+                        <p><div className="p_bold">Main Tech Stack : </div>
+                        <Badge badgeName="Nodejs" badgeLogo="Node.js" badgeColor="white" badgeKind="square">node.js-339933</Badge>
+                        <Badge badgeName="Mysql" badgeLogo="MySql" badgeColor="white" badgeKind="square">Mysql-E6B91E</Badge>
+                        </p>
                         <br/>
                         <p><a href="https://github.com/wq0212/campus-compass" 
                         target="_blank"
@@ -26,36 +27,26 @@ class MobileSection3 extends Component{
                         rel="noopener noreferrer">Blog Review</a></p>
                         
                     </div>
-                    <div className="projectBox rightBox">
+                    <div className="projectBoxM">
                         <h4>Blog Create</h4>
-                        <br/>
-                        <p>Make own blog project use Django (2021.1 ~ 2021.6)</p>
-                        <p><div className="p_bold">Main Tech Stack</div> : Django, Docker</p>
-                        <p><div className="p_bold">Summary</div> : 처음으로 웹개발을 공부하며 만들기 시작한 Django 기반 카페형 블로그</p>
+                        <p>Make own blog project use Django</p>
+                        <p><div className="p_bold">Main Tech Stack : </div>
+                        <Badge badgeName="Django" badgeLogo="Django" badgeColor="white" badgeKind="square">Django-092E20</Badge>
+                        <Badge badgeName="Docker" badgeLogo="docker" badgeColor="white" badgeKind="square">docker-%230db7ed.svg</Badge>
+                        </p>
                         <br/>
                         <p><a href="https://github.com/wq0212/BlogCreate" 
                         target="_blank" 
                         rel="noopener noreferrer">Github</a></p>
 
                     </div>
-                    <div className="projectBox leftBox">
-                        <h4>Engineering Board</h4>
-                        <br/>
-                        <p>Sogang Univ. Engineering Student Council Board</p>
-                        <p><div className="p_bold">Main Tech Stack</div> : Node.js + express, Mysql</p>
-                        <p><div className="p_bold">Summary</div> : 처음으로 git을 이용해 협업을 하면서 개발한 공학부 신문고 게시판</p>
-                        <br/>
-                        <p><a href="https://github.com/wq0212/SGEngineering-Board" 
-                        target="_blank" 
-                        rel="noopener noreferrer">Github</a></p>
-                        
-                    </div>
-                    <div className="projectBox rightBox">
+                    <div className="projectBoxM">
                         <h4>Profile Page</h4>
-                        <br/>
+
                         <p>Make own profile page use react</p>
-                        <p><div className="p_bold">Main Tech Stack</div> : React</p>
-                        <p><div className="p_bold">Summary</div> : 지금 보고 계시는 프로필 페이지</p>
+                        <p><div className="p_bold">Main Tech Stack : </div>
+                        <Badge badgeName="React" badgeLogo="react" badgeColor="61DAFB" badgeKind="square">React-20232A</Badge>
+                        </p>
                         <br/>
                         <p><a href="https://github.com/wq0212/profile-page-react" 
                         target="_blank" 
