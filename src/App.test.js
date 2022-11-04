8b85ca276999d950efbcf0faee017d976af6ad94
ShinDongJun <wq0212@naver.com>
2022-11-05 02:44:06 +0900

    feat: 폰트적용 및 header 수정

--- src/App.test.js
+++
@@ -1,8 +0,0 @@
-import { render, screen } from '@testing-library/react';
-import FullPageLanding from './App';
-
-test('renders learn react link', () => {
-  render(<FullPageLanding />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
--- src/component/mobile_Header.js
+++ src/component/mobile_Header.js
@@ -1,38 +1,42 @@
-import React,{ Component } from "react";
-import '../css/mobile_Header.css';
-import { FaYoutube,FaGithub,FaBloggerB } from "react-icons/fa";
-class Header extends Component{
-    render(){
-        return (
-            <div className="headerM">
-                <a href="https://www.youtube.com/channel/UCXgIxJNWOp3E6RfYcUuaH3g"
-                target="_blank" 
-                rel="noopener noreferrer">
-                    <div className="youtubeButton buttonBox">
-                        <FaYoutube size={30}/>
-                    </div>
-                </a>
+import React, { Component } from "react";
+import "../css/mobile_Header.css";
+import { FaLinkedin, FaGithub, FaBloggerB } from "react-icons/fa";
+class Header extends Component {
+  render() {
+    return (
+      <div className="headerM">
+        <a
+          href="https://www.youtube.com/channel/UCXgIxJNWOp3E6RfYcUuaH3g"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <div className="youtubeButton buttonBox">
+            <FaLinkedin size={30} />
+          </div>
+        </a>
 
-                <a href="https://github.com/wq0212"
-                target="_blank" 
-                rel="noopener noreferrer">
-                    <div className="githubButton buttonBox">
-                        <FaGithub size={30}/>
-                    </div>
-                </a>
+        <a
+          href="https://github.com/wq0212"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <div className="githubButton buttonBox">
+            <FaGithub size={30} />
+          </div>
+        </a>
 
-                <a href="https://pypystory.tistory.com/"
-                target="_blank" 
-                rel="noopener noreferrer">
-                    <div className="blogButton buttonBox">
-                        <FaBloggerB size={30}/>
-                    </div>
-                </a>
-                
-            </div>
-            
-        )
-    }
+        <a
+          href="https://pypystory.tistory.com/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <div className="blogButton buttonBox">
+            <FaBloggerB size={30} />
+          </div>
+        </a>
+      </div>
+    );
+  }
 }
 
 export default Header;
