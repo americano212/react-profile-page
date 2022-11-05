273702b7e41cd94c5e38cfa0fbc512031b5670f4
ShinDongJun <wq0212@naver.com>
2022-11-05 17:23:34 +0900

    feat: linkedin 로고추가

--- src/component/mainpage_section_4.js
+++ src/component/mainpage_section_4.js
@@ -1,33 +1,111 @@
-import React,{ Component } from "react";
-import '../css/fullpage-overrides.css';
-import '../css/mainpage_section_4.css';
+import React, { Component } from "react";
+import "../css/fullpage-overrides.css";
+import "../css/mainpage_section_4.css";
 
 import Logo from "./ui/logo";
 
-class FullPageSection4 extends Component{
-    render(){
-        return (
-            <div className="section">
-                <div className="contactWrap">
-                    <h1>Contacts</h1>
-                    <div className="logoBox">                    
-                    <Logo logoName="Facebook" logoPath="img/icon/facebook.svg" logoLink="https://www.facebook.com/profile.php?id=100010290605875" background="#4267B2">신동준</Logo>
-                    <Logo logoName="Instagram" logoPath="img/icon/instagram.svg" logoLink="https://www.instagram.com/dongjun0212/?hl=ko" background="#C13584">dongjun0212</Logo>
-                    <Logo logoName="Blog" logoPath="img/icon/blog.svg" logoLink="https://pypystory.tistory.com/" background="#eb531f">PyPyStory</Logo>
-                    <Logo logoName="Github" logoPath="img/icon/github.svg" logoLink="https://github.com/wq0212" background="#333">wq0212</Logo>
-                    <Logo logoName="Youtube" logoPath="img/icon/youtube.svg" logoLink="https://www.youtube.com/channel/UCXgIxJNWOp3E6RfYcUuaH3g" background="#FF0000">PyPyStory</Logo>
-                    <Logo logoName="Mail" logoPath="img/icon/gmail.svg" logoLink="#" background="#EA4335">wq0212<br />@naver.com</Logo>
-                    <Logo logoName="Kakao Talk" logoPath="img/icon/kakao.svg" logoLink="https://open.kakao.com/o/skoddvae" background="#FFE812">Open Talk</Logo>
-                    </div>
-                    <div className="logoBox">
-                    <Logo logoName="Solved AC" logoPath="img/icon/solvedac.svg" logoLink="https://solved.ac/profile/wq0212" background="#17CE3A" logoSize="100" logoTop="0px">wq0212</Logo>
-                    <Logo logoName="Baekjoon" logoPath="img/icon/boj.svg" logoLink="https://www.acmicpc.net/user/wq0212" background="#0077C1" logoSize="100" logoTop="0px">wq0212</Logo>
-                    </div>
-                    
-                </div>
-            </div>
-        )
-    }
+class FullPageSection4 extends Component {
+  render() {
+    return (
+      <div className="section">
+        <div className="contactWrap">
+          <h1>Contacts</h1>
+          <div className="logoBox">
+            <Logo
+              logoName="Facebook"
+              logoPath="img/icon/facebook.svg"
+              logoLink="https://www.facebook.com/profile.php?id=100010290605875"
+              background="#4267B2"
+            >
+              신동준
+            </Logo>
+            <Logo
+              logoName="Instagram"
+              logoPath="img/icon/instagram.svg"
+              logoLink="https://www.instagram.com/dongjun0212/?hl=ko"
+              background="#C13584"
+            >
+              dongjun0212
+            </Logo>
+            <Logo
+              logoName="Blog"
+              logoPath="img/icon/blog.svg"
+              logoLink="https://pypystory.tistory.com/"
+              background="#eb531f"
+            >
+              PyPyStory
+            </Logo>
+            <Logo
+              logoName="Github"
+              logoPath="img/icon/github.svg"
+              logoLink="https://github.com/wq0212"
+              background="#333"
+            >
+              wq0212
+            </Logo>
+            <Logo
+              logoName="Youtube"
+              logoPath="img/icon/youtube.svg"
+              logoLink="https://www.youtube.com/channel/UCXgIxJNWOp3E6RfYcUuaH3g"
+              background="#FF0000"
+            >
+              PyPyStory
+            </Logo>
+            <Logo
+              logoName="Mail"
+              logoPath="img/icon/gmail.svg"
+              logoLink="#"
+              background="#EA4335"
+            >
+              wq0212
+              <br />
+              @naver.com
+            </Logo>
+            <Logo
+              logoName="Kakao Talk"
+              logoPath="img/icon/kakao.svg"
+              logoLink="https://open.kakao.com/o/skoddvae"
+              background="#FFE812"
+            >
+              Open Talk
+            </Logo>
+          </div>
+          <div className="logoBox">
+            <Logo
+              logoName="Solved AC"
+              logoPath="img/icon/solvedac.svg"
+              logoLink="https://solved.ac/profile/wq0212"
+              background="#17CE3A"
+              logoSize="100"
+              logoTop="0px"
+            >
+              wq0212
+            </Logo>
+            <Logo
+              logoName="Baekjoon"
+              logoPath="img/icon/boj.svg"
+              logoLink="https://www.acmicpc.net/user/wq0212"
+              background="#0077C1"
+              logoSize="100"
+              logoTop="0px"
+            >
+              wq0212
+            </Logo>
+            <Logo
+              logoName="LinkedIn"
+              logoPath="img/icon/linkedin.svg"
+              logoLink="https://www.linkedin.com/in/americano212/"
+              background="#0077B5"
+              logoSize="50"
+              logoTop="25px"
+            >
+              americano212
+            </Logo>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
-export default FullPageSection4
+export default FullPageSection4;
--- src/component/mobile_section_4.js
+++ src/component/mobile_section_4.js
@@ -1,33 +1,91 @@
-import React,{ Component } from "react";
-import '../css/fullpage-overrides.css';
-import '../css/mobile_section_4.css';
+import React, { Component } from "react";
+import "../css/fullpage-overrides.css";
+import "../css/mobile_section_4.css";
 
 import Logo from "./ui/logo";
 
-class MobileSection4 extends Component{
-    render(){
-        return (
-            <div className="section">
-                <div className="contactWrapM">
-                    <div className="contactTitleBoxM"><h1>Contacts</h1></div>
-                    <div className="logoBoxM">                    
-                    <Logo logoName="Facebook" logoPath="img/icon/facebook.svg" logoLink="https://www.facebook.com/profile.php?id=100010290605875" background="#4267B2"></Logo>
-                    <Logo logoName="Instagram" logoPath="img/icon/instagram.svg" logoLink="https://www.instagram.com/dongjun0212/?hl=ko" background="#C13584"></Logo>
-                    <Logo logoName="Blog" logoPath="img/icon/blog.svg" logoLink="https://pypystory.tistory.com/" background="#eb531f"></Logo>
-                    <Logo logoName="Github" logoPath="img/icon/github.svg" logoLink="https://github.com/wq0212" background="#333"></Logo>
-                    <Logo logoName="Youtube" logoPath="img/icon/youtube.svg" logoLink="https://www.youtube.com/channel/UCXgIxJNWOp3E6RfYcUuaH3g" background="#FF0000"></Logo>
-                    <Logo logoName="Mail" logoPath="img/icon/gmail.svg" logoLink="#" background="#EA4335"></Logo>
-                    <Logo logoName="Kakao Talk" logoPath="img/icon/kakao.svg" logoLink="https://open.kakao.com/o/skoddvae" background="#FFE812"></Logo>
-                    </div>
-                    <div className="logoBoxM">
-                    <Logo logoName="Solved AC" logoPath="img/icon/solvedac.svg" logoLink="https://solved.ac/profile/wq0212" background="#17CE3A" logoSize="100" logoTop="0px"></Logo>
-                    <Logo logoName="Baekjoon" logoPath="img/icon/boj.svg" logoLink="https://www.acmicpc.net/user/wq0212" background="#0077C1" logoSize="100" logoTop="0px"></Logo>
-                    </div>
-                    
-                </div>
-            </div>
-        )
-    }
+class MobileSection4 extends Component {
+  render() {
+    return (
+      <div className="section">
+        <div className="contactWrapM">
+          <div className="contactTitleBoxM">
+            <h1>Contacts</h1>
+          </div>
+          <div className="logoBoxM">
+            <Logo
+              logoName="Facebook"
+              logoPath="img/icon/facebook.svg"
+              logoLink="https://www.facebook.com/profile.php?id=100010290605875"
+              background="#4267B2"
+            ></Logo>
+            <Logo
+              logoName="Instagram"
+              logoPath="img/icon/instagram.svg"
+              logoLink="https://www.instagram.com/dongjun0212/?hl=ko"
+              background="#C13584"
+            ></Logo>
+            <Logo
+              logoName="Blog"
+              logoPath="img/icon/blog.svg"
+              logoLink="https://pypystory.tistory.com/"
+              background="#eb531f"
+            ></Logo>
+            <Logo
+              logoName="Github"
+              logoPath="img/icon/github.svg"
+              logoLink="https://github.com/wq0212"
+              background="#333"
+            ></Logo>
+            <Logo
+              logoName="Youtube"
+              logoPath="img/icon/youtube.svg"
+              logoLink="https://www.youtube.com/channel/UCXgIxJNWOp3E6RfYcUuaH3g"
+              background="#FF0000"
+            ></Logo>
+            <Logo
+              logoName="Mail"
+              logoPath="img/icon/gmail.svg"
+              logoLink="#"
+              background="#EA4335"
+            ></Logo>
+            <Logo
+              logoName="Kakao Talk"
+              logoPath="img/icon/kakao.svg"
+              logoLink="https://open.kakao.com/o/skoddvae"
+              background="#FFE812"
+            ></Logo>
+          </div>
+          <div className="logoBoxM">
+            <Logo
+              logoName="Solved AC"
+              logoPath="img/icon/solvedac.svg"
+              logoLink="https://solved.ac/profile/wq0212"
+              background="#17CE3A"
+              logoSize="100"
+              logoTop="0px"
+            ></Logo>
+            <Logo
+              logoName="Baekjoon"
+              logoPath="img/icon/boj.svg"
+              logoLink="https://www.acmicpc.net/user/wq0212"
+              background="#0077C1"
+              logoSize="100"
+              logoTop="0px"
+            ></Logo>
+            <Logo
+              logoName="LinkedIn"
+              logoPath="img/icon/linkedin.svg"
+              logoLink="https://www.linkedin.com/in/americano212/"
+              background="#0077B5"
+              logoSize="50"
+              logoTop="25px"
+            ></Logo>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
-export default MobileSection4
+export default MobileSection4;
